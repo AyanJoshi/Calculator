54265acacde7fd808d75a11f1a8cc31822536754
Ayan Joshi <ajoshi27@asu.edu>
2019-02-22 12:21:04 -0700

    Add a simple output change

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -12,26 +12,29 @@ public class Calculator {
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
-	}
+	}	
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total+= value;
 	}
 	
 	public void subtract (int value) {
-		
+		total-= value;
 	}
 	
 	public void multiply (int value) {
-		
+		total*= value;
 	}
 	
 	public void divide (int value) {
-		
+		if(value == 0)
+			total = 0;
+		else
+			total = total/value;
 	}
 	
 	public String getHistory () {
