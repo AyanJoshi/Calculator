634e8d523b4af8037b273c7f451045a27ba23b9b
Ayan Joshi <Ayan Joshi@DESKTOP-EK9BR6M.3500.dhcp.asu.edu>
2019-02-22 12:03:31 -0700

    Initial checking of calculator.

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,40 @@
+/*
+Name: Ayan Joshi
+Class ID: 429
+Description: This is a calculator class with functions to
+	 		 to perform basic mathematical functions.
+ */
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
